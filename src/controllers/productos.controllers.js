4c74bf275c43b2ba6961ee115cf51b8dcbc3db8e
Celina <idkmahee@gmail.com>
2025-10-08 12:45:18 -0300

    agregue modelo Producto y borre console log del controlador de crearProducto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,5 @@
+import Producto from "../models/producto.js";
+
 export const test = (req, res) => {
   res.status(200);
   res.send("Primera prueba desde el backend");
@@ -5,11 +7,13 @@ export const test = (req, res) => {
 
 export const leerProductos = (req, res) => {};
 
-export const CrearProducto = (req, res) => {
+export const CrearProducto = async (req, res) => {
   try {
-    console.log(req);
-    console.log(req.body);
+    const NuevoProducto = new Producto(req.body);
+    await NuevoProducto.save();
+    res.status(201).json({ mensaje: "Producto creado correctamente" });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ mensaje: "Error al crear el producto" });
   }
 };
