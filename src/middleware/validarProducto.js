0ad56efad5f8f7d03c5db0aca2a0b231f0203918
Celina <idkmahee@gmail.com>
2025-10-13 16:59:49 -0300

    agregue validaciones para precio y nombre de productos utilizando express-validator

---
+++ src/middleware/validarProducto.js
@@ -0,0 +1,36 @@
+import { body } from "express-validator";
+import resultadoValidacion from "./resultadoValidacion.js";
+import Producto from "../models/producto.js";
+
+const validacionProducto = [
+  body("nombreProducto")
+    .notEmpty()
+    .withMessage("El nombre del producto es obligatorio")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("El nombre del producto debe tener entre 2 y 100 caracteres")
+    .custom(async (valor, { req }) => {
+      const productoExistente = await Producto.findOne({
+        nombreProducto: valor,
+      });
+      if (!productoExistente) return true;
+      if (req.params?.id && productoExistente._id.toString() === req.params.id)
+        return true;
+
+      throw new Error("Ya existe un producto con ese nombre");
+    }),
+  body("precio")
+    .notEmpty()
+    .withMessage("El precio es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un número")
+    .custom((valor) => {
+      if (valor >= 50 && valor <= 1000000) {
+        return true;
+      } else {
+        throw new Error("El precio debe estar entre 50 y 1000000");
+      }
+    }),
+  (req, res, next) => resultadoValidacion(req, res, next),
+];
+
+export default validacionProducto;
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -7,15 +7,16 @@ import {
   leerProductos,
   test,
 } from "../controllers/productos.controllers.js";
+import validacionProducto from "../middleware/validarProducto.js";
 
 const router = Router();
 
 router.route("/test").get(test);
-router.route("/").get(leerProductos).post(CrearProducto);
+router.route("/").get(leerProductos).post(validacionProducto, CrearProducto);
 router
   .route("/:id")
   .get(leerProductoPorId)
   .delete(borrarProductoPorId)
-  .put(editarProductoPorId);
+  .put(validacionProducto, editarProductoPorId);
 
 export default router;
